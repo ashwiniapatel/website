d6cb224748406679486962a038be162d60660070
Amit-Patel <amitpatel111297@gmail.com>
2024-05-29 01:19:50 +0300

    initial setup

---
+++ vivweld-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World! Welcome to VivWeld's website. <br></br>We are under construction. Stay tuned...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
